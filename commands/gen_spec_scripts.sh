f8c2dcef9394997a5dd709f44a4b5bd8b4ca8d84
David Biancolin <david.biancolin@gmail.com>
2017-03-10 14:56:38 -0800

    Make shit work for David

---
+++ commands/gen_spec_scripts.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#set -e
+INPUT_TYPE=test
+#INPUT_TYPE=ref
+COMMAND_DIR=../benchmarks/spec-${INPUT_TYPE}/commands
+OUTPUT_SUBDIR=`pwd`/spec
+
+# the integer set
+BENCHMARKS=(400.perlbench 401.bzip2 403.gcc 429.mcf 445.gobmk 456.hmmer 458.sjeng 462.libquantum 464.h264ref 471.omnetpp 473.astar 483.xalancbmk)
+
+
+QSUB_COMMAND_FILE=spec.$INPUT_TYPE.txt
+if [[ -e $QSUB_COMMAND_FILE ]]; then
+echo "Removing existing $QSUB_COMMAND_FILE"
+rm $QSUB_COMMAND_FILE
+fi
+
+mkdir -p $OUTPUT_SUBDIR
+
+for b in ${BENCHMARKS[@]}; do
+
+  SHORT_EXE=${b##*.} # cut off the numbers ###.short_exe
+  # handle benchmarks that don't conform to the naming convention
+  if [ $b == "482.sphinx3" ]; then SHORT_EXE=sphinx_livepretend; fi
+  if [ $b == "483.xalancbmk" ]; then SHORT_EXE=Xalan; fi
+
+  # read the command file
+  IFS=$'\n' read -d '' -r -a commands < $COMMAND_DIR/${b}.${INPUT_TYPE}.cmd
+
+  PROFILE_FILE=${OUTPUT_SUBDIR}/${b}.${INPUT_TYPE}.txt
+  if [[ -e $PROFILE_FILE ]]; then
+    echo "Removing existing $PROFILE_FILE"
+    rm $PROFILE_FILE
+  fi
+
+  # Write the benchmark to the qsub command file
+  echo "${b}.${INPUT_TYPE} # spec-${INPUT_TYPE}/${b} # FILE:${PROFILE_FILE}" >> $QSUB_COMMAND_FILE
+
+  echo "Generating ${PROFILE_FILE}"
+  for input in "${commands[@]}"; do
+     if [[ ${input:0:1} != '#' ]]; then # allow us to comment out lines in the cmd files
+        echo "./${SHORT_EXE} $input" >> $PROFILE_FILE
+     fi
+  done
+
+done
+
+
+echo ""
+echo "Done!"
--- spawn.py
+++ spawn.py
@@ -13,19 +13,25 @@ import os
 from datetime import datetime
 
 #FPGA_BITSTREAM="rocketchip_wrapper_mig_hpmss.bit"
-DEFAULT_FPGA_BITSTREAM="bitfiles/boomchip_wrapper.bit"
-LINUX_SOURCE="/scratch/celio/linux-flow"
-INITFS_SOURCE="/nscratch/celio/initram"
-BMARK_SOURCE="/nscratch/celio/qsub-fpga-initramfs/benchmarks"
+DEFAULT_FPGA_BITSTREAM="bitfiles/midas_wrapper.bit"
+LINUX_SOURCE="/scratch/biancolin/initramfs_linux_flow"
+INITFS_SOURCE="/nscratch/biancolin/initram"
+BMARK_SOURCE="benchmarks"
 OUTPUT_DIR="output"
 
 EMAIL_ENABLED=True
+
+ENABLE_GCC=False
+ENABLE_BASH=False
+ENABLE_PYTHON=False
 
 def main():
     parser = optparse.OptionParser()
     parser.add_option('-f', '--file', dest='filename', help='input command file')
     parser.add_option('-b', '--bitstream', dest='bitstream', help='input bitstream file')
     parser.add_option('-o', '--outdir', dest='outdir', help='output directory')
+    parser.add_option('-d', '--disable_counters', dest='disable_counters', default=False,
+                      action="store_true", help='Does not run rv_counters in target machine.')
     (options, args) = parser.parse_args()
     if not options.filename:
         parser.error('Please give an input filename with -f')
@@ -43,19 +49,26 @@ def main():
         OUTPUT_DIR = OUTPUT_DIR + "-" + "boom"
     print OUTPUT_DIR
 
-
+    # Each line in the command file represents a different ramdisk image and
+    # thus the basic unit of qsub-task parallelism
     for line in f:
         if line[0] == '#': continue
         (bmk_str, dir_str, cmd_str) = line.split("#")
         bmk_str = bmk_str.strip()
         dir_str = dir_str.strip()
-        cmd_str = cmd_str.lstrip()
+        cmd_str = cmd_str.lstrip().strip()
+        # For come complicated benchmarks consisting of multiple shell commands
+        # a file path can be given inplace of a single shell command
+        if (cmd_str.startswith("FILE:")):
+            with open(cmd_str[5:], 'r') as cmd_file:
+                cmd_str = ''.join(cmd_file.readlines())
+
         print "Benchmark  : ", bmk_str
         p = subprocess.Popen("mkdir -p build", stdout=subprocess.PIPE, shell=True)
         qfile = 'build/qcmd_' + bmk_str + ".sh"
         initfile = 'build/init_profile_' + bmk_str
-        print cmd_str,
-        generate_init_file(cmd_str, dir_str, initfile)
+        print cmd_str
+        generate_init_file(cmd_str, dir_str, initfile, options.disable_counters)
         generate_bblvmlinux(bmk_str, dir_str, initfile)
         linux = 'build/bblvmlinux-' + bmk_str
         generate_qsub_file(bmk_str, cmd_str, qfile, OUTPUT_DIR, linux, fpga_bitstream)
@@ -67,47 +80,50 @@ def main():
 
 
 #---------------------
-def generate_init_file(cmd_str, dir_str, initfile):
+def generate_init_file(cmd_str, dir_str, initfile, disable_counters):
     print "Opening initfile: ", initfile
     with open(initfile, 'w') as f:
         f.write("echo \"\"\n")
         f.write("uname -a\n")
         f.write("echo \"\"\n")
-
-        f.write("export PATH=$PATH:/usr/libexec/gcc/riscv64-poky-linux/6.1.1\n")
-        f.write("export PYTHONHOME=/usr\n")
-        f.write("export PYTHONPATH=/usr/lib/python2.7\n")
+        if ENABLE_PYTHON:
+            f.write("export PATH=$PATH:/usr/libexec/gcc/riscv64-poky-linux/6.1.1\n")
+            f.write("export PYTHONHOME=/usr\n")
+            f.write("export PYTHONPATH=/usr/lib/python2.7\n")
         f.write("cd /usr/bin\n")
-        f.write("ln -s riscv64-poky-linux-addr2line addr2line\n")
-        f.write("ln -s riscv64-poky-linux-ar ar\n")
-        f.write("ln -s riscv64-poky-linux-as as\n")
-        f.write("ln -s riscv64-poky-linux-c++filt c++filt\n")
-        f.write("ln -s riscv64-poky-linux-elfedit elfedit\n")
-        f.write("ln -s riscv64-poky-linux-gcc gcc\n")
-        f.write("ln -s riscv64-poky-linux-gprof gprof\n")
-        f.write("ln -s riscv64-poky-linux-ld ld\n")
-        f.write("ln -s riscv64-poky-linux-ld.bfd ld.bfd\n")
-        f.write("ln -s riscv64-poky-linux-nm nm\n")
-        f.write("ln -s riscv64-poky-linux-objcopy objcopy\n")
-        f.write("ln -s riscv64-poky-linux-objdump objdump\n")
-        f.write("ln -s riscv64-poky-linux-ranlib ranlib\n")
-        f.write("ln -s riscv64-poky-linux-readelf readlef\n")
-        f.write("ln -s riscv64-poky-linux-size size\n")
-        f.write("ln -s riscv64-poky-linux-strings strings\n")
-        f.write("ln -s riscv64-poky-linux-strip strip\n")
-        f.write("\n")
-        f.write("ln -s python2.7 python2\n")
-        f.write("ln -s python2 python\n")
-        f.write("ln -s python2.7-config python2-config\n")
-        f.write("ln -s python2-config python-config\n")
+        if ENABLE_GCC:
+            f.write("ln -s riscv64-poky-linux-addr2line addr2line\n")
+            f.write("ln -s riscv64-poky-linux-ar ar\n")
+            f.write("ln -s riscv64-poky-linux-as as\n")
+            f.write("ln -s riscv64-poky-linux-c++filt c++filt\n")
+            f.write("ln -s riscv64-poky-linux-elfedit elfedit\n")
+            f.write("ln -s riscv64-poky-linux-gcc gcc\n")
+            f.write("ln -s riscv64-poky-linux-gprof gprof\n")
+            f.write("ln -s riscv64-poky-linux-ld ld\n")
+            f.write("ln -s riscv64-poky-linux-ld.bfd ld.bfd\n")
+            f.write("ln -s riscv64-poky-linux-nm nm\n")
+            f.write("ln -s riscv64-poky-linux-objcopy objcopy\n")
+            f.write("ln -s riscv64-poky-linux-objdump objdump\n")
+            f.write("ln -s riscv64-poky-linux-ranlib ranlib\n")
+            f.write("ln -s riscv64-poky-linux-readelf readlef\n")
+            f.write("ln -s riscv64-poky-linux-size size\n")
+            f.write("ln -s riscv64-poky-linux-strings strings\n")
+            f.write("ln -s riscv64-poky-linux-strip strip\n")
+        if ENABLE_PYTHON:
+            f.write("\n")
+            f.write("ln -s python2.7 python2\n")
+            f.write("ln -s python2 python\n")
+            f.write("ln -s python2.7-config python2-config\n")
+            f.write("ln -s python2-config python-config\n")
         f.write("cd ~\n")
 
         f.write("ls -ls /bin\n")
         f.write("ls -ls /usr/bin\n")
-        f.write("cd /celio/" + dir_str + "\n")
+        f.write("cd /celio/" + os.path.basename(dir_str) + "\n")
         f.write("ls\n")
 #        f.write("/celio/rv_counters_hpm -c &\n")
-        f.write("/celio/rv_counters_hpm &\n")
+        if not disable_counters:
+            f.write("/celio/rv_counters_hpm &\n")
         f.write("sleep 1\n")
         f.write(cmd_str)
         f.write("killall rv_counters_hpm\n")
@@ -119,13 +135,14 @@ def generate_init_file(cmd_str, dir_str, initfile):
 #---------------------
 def generate_bblvmlinux(bmk_str, dir_str, initfile):
     print "Generating bblvmlinux with: ", initfile
-    target = INITFS_SOURCE + "/" + dir_str
+    target = INITFS_SOURCE + "/" + os.path.basename(dir_str)
     cmds =""
     cmds += "\n"
     cmds += "cp " + initfile + " " + LINUX_SOURCE + "/profile\n"
-    cmds += "cp -r " + BMARK_SOURCE + "/" + dir_str + " " + target + "\n"
+    cmds += "cp -rf " + BMARK_SOURCE + "/" + dir_str + " " + target + "\n"
+    cmds += "cd " + LINUX_SOURCE + "; ./build-initram.py\n"
     cmds += "cd " + LINUX_SOURCE + "; make\n"
-    cmds += "cp " + LINUX_SOURCE + "/bblvmlinux /nscratch/celio/qsub-fpga-initramfs/build/bblvmlinux-" + bmk_str + "\n"
+    cmds += "cp " + LINUX_SOURCE + "/bblvmlinux /nscratch/" + getpass.getuser() + "/qsub-fpga-initramfs/build/bblvmlinux-" + bmk_str + "\n"
     cmds += "rm -r " + target + "\n"
     print cmds
     p = subprocess.Popen(cmds, stdout=subprocess.PIPE, shell=True)
@@ -136,9 +153,9 @@ def generate_bblvmlinux(bmk_str, dir_str, initfile):
 #---------------------
 def generate_qsub_file(bmk_str, cmd_str, qfile, output_dir, linux, fpga_bitstream):
     with open(qfile, 'w') as f:
+        # I can't get this to work with sh for some reason
+        f.write("#!/bin/bash\n")
         f.write("### This file was auto-generated by spawn.py\n")
-            
-        f.write("#!/bin/sh\n")
         f.write("### Set the job name\n")
         f.write("#PBS -N " + bmk_str + "\n\n")
         f.write("### Declare myprogram non-rerunable\n")
@@ -186,9 +203,9 @@ def generate_qsub_file(bmk_str, cmd_str, qfile, output_dir, linux, fpga_bitstrea
         ### FPGA stuff
         f.write("### FPGA Stuff\n")
         f.write("source /nscratch/fpga-cluster/fpga-scripts/aspire-fpga-cluster-select.sh\n")
+        f.write("sleep 1\n")
         f.write("echo \"FPGA IP:\"\n")
         f.write("echo $FPGA_IP\n")
-        
         f.write("### Load the fpga bitfile\n")
         f.write("source /ecad/tools/xilinx/Vivado/2016.2/settings64.sh\n")
         f.write("which vivado\n")
@@ -197,10 +214,10 @@ def generate_qsub_file(bmk_str, cmd_str, qfile, output_dir, linux, fpga_bitstrea
         f.write("sleep 2\n")
         
         f.write("### Send over the image we want to the FPGA\n")
-        f.write("scp -i ./ssh/id_rsa /nscratch/$USER/qsub-fpga-initramfs/" + linux + " root@$FPGA_IP:/sdcard/$USER/linux\n")
-        f.write("scp -i ./ssh/id_rsa /nscratch/$USER/qsub-fpga-initramfs/libfesvr.so root@$FPGA_IP:/sdcard/$USER/libfesvr.so\n")
-        f.write("scp -i ./ssh/id_rsa /nscratch/$USER/qsub-fpga-initramfs/fesvr-zynq root@$FPGA_IP:/sdcard/$USER/fesvr-zynq\n")
-        f.write("scp -i ./ssh/id_rsa /nscratch/$USER/qsub-fpga-initramfs/pk root@$FPGA_IP:/sdcard/$USER/pk\n")
+        f.write("scp -i ~/.ssh/id_rsa /nscratch/$USER/qsub-fpga-initramfs/" + linux + " root@$FPGA_IP:/sdcard/$USER/linux\n")
+        f.write("scp -i ~/.ssh/id_rsa /nscratch/$USER/qsub-fpga-initramfs/libfesvr.so root@$FPGA_IP:/sdcard/$USER/libfesvr.so\n")
+        f.write("scp -i ~/.ssh/id_rsa /nscratch/$USER/qsub-fpga-initramfs/fesvr-zynq root@$FPGA_IP:/sdcard/$USER/fesvr-zynq\n")
+        f.write("scp -i ~/.ssh/id_rsa /nscratch/$USER/qsub-fpga-initramfs/pk root@$FPGA_IP:/sdcard/$USER/pk\n")
         f.write("sleep 2\n")
 
         f.write("### Log-in to the FPGA and run the benchmark\n")
